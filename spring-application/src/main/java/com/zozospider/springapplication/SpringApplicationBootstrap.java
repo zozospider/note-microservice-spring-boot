86e75f20fb6be201dcb9b8ae83f083d0d00ccaca
zozospider <41692211+zozospider@users.noreply.github.com>
2018-10-16 22:52:15 +0800

    添加spring-application模块

---
+++ spring-application/src/main/java/com/zozospider/springapplication/SpringApplicationBootstrap.java
@@ -0,0 +1,17 @@
+package com.zozospider.springapplication;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * {@link SpringApplication} 引导类
+ *
+ * @author zozo
+ * @since 1.0
+ */
+public class SpringApplicationBootstrap {
+
+    public static void main(String[] args) {
+
+    }
+
+}
