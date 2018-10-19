dcf002a71b69fe565d23f1c6cfc1baa2be9d8ce7
zozospider <41692211+zozospider@users.noreply.github.com>
2018-10-19 21:10:15 +0800

    update spring-application

--- spring-application/src/main/java/com/zozospider/springapplication/listener/FirstApplicationListener.java
+++ spring-application/src/main/java/com/zozospider/springapplication/listener/FirstApplicationListener.java
@@ -2,6 +2,8 @@ package com.zozospider.springapplication.listener;
 
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 
 /**
  * First {@link ApplicationListener} 监听 {@link ContextRefreshedEvent}
@@ -9,6 +11,7 @@ import org.springframework.context.event.ContextRefreshedEvent;
  * @author zozo
  * @since 1.0
  */
+@Order(Ordered.HIGHEST_PRECEDENCE) // 进行排序，并指定为高级别
 public class FirstApplicationListener implements ApplicationListener<ContextRefreshedEvent> {
 
     @Override
--- spring-application/src/main/java/com/zozospider/springapplication/listener/SecondApplicationListener.java
+++ spring-application/src/main/java/com/zozospider/springapplication/listener/SecondApplicationListener.java
@@ -2,6 +2,7 @@ package com.zozospider.springapplication.listener;
 
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.Ordered;
 
 /**
  * Second {@link ApplicationListener} 监听 {@link ContextRefreshedEvent}
@@ -9,12 +10,22 @@ import org.springframework.context.event.ContextRefreshedEvent;
  * @author zozo
  * @since 1.0
  */
-public class SecondApplicationListener implements ApplicationListener<ContextRefreshedEvent> {
+public class SecondApplicationListener implements ApplicationListener<ContextRefreshedEvent>, Ordered {
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         System.out.println("Second ApplicationListener: " + event.getApplicationContext().getId()
                 + ", timestamp: " + event.getTimestamp());
+    }
+
+    /**
+     * 实现 Ordered 接口，进行排序，并指定为低级别
+     *
+     * @return
+     */
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE;
     }
 
 }
